794393b4249b8e3ca0f2099076fca3692776c042
Aron Forero <Foreroo965@gmail.com>
2019-01-07 22:34:30 -0500

    First approach to swagger, look up for improvements and how to do tests.

--- houses/urls.py
+++ houses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from rest_framework_swagger.views import get_swagger_view
 
 router = DefaultRouter()
 
@@ -9,6 +10,9 @@ router.register(r'general', views.generalViewSet)
 router.register(r'interior', views.interiorViewSet)
 router.register(r'exterior', views.exteriorViewSet)
 
+schema_view = get_swagger_view(title='Pastebin API')
+
 urlpatterns = [
+    path('api/doc/', schema_view),
     path('', include(router.urls)),
 ]
